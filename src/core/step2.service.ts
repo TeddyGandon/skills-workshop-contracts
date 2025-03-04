8f8ef27a87a8474c22f40d07bb87ac243c5736e9
TeddyGandon <teddy@coretizone.com>
2025-03-04 21:31:13 +0530

    Update step2.service.ts

--- src/core/step2.service.ts
+++ src/core/step2.service.ts
@@ -2,6 +2,6 @@ import { IService } from "./service.interface";
 
 export class Step2Service implements IService {
     forward(param: any): any {
-        throw new Error("Method not implemented.");
+        return param;
     }
 }
